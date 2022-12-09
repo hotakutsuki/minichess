c7f001076fbad4eae7f11da8b1674f28f8de1272
Josue Ortiz <jotakutsuki@gmail.com>
2022-12-09 02:53:14 -0500

    fixed tutorial build for web

--- lib/screens/Tutorial.dart
+++ lib/screens/Tutorial.dart
@@ -12,13 +12,16 @@ class _TutorialState extends State<Tutorial> {
     initialPage: 0,
   );
 
-  bool diablePrev = false, disableNext = false;
+  bool diablePrev = true, disableNext = false;
 
   @override
   void initState() {
     controller.addListener(() {
-      diablePrev = controller.page == 0;
-      disableNext = controller.page == 8;
+      setState(() {
+        diablePrev = controller.page == 0;
+        disableNext = controller.page == 8;
+      });
+
     });
     super.initState();
   }
@@ -28,16 +31,22 @@ class _TutorialState extends State<Tutorial> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
-          height: 500,
-          padding: const EdgeInsets.all(48.0),
+          decoration: BoxDecoration(
+            border: Border.all(),
+          ),
+          height: 300,
+          padding: const EdgeInsets.all(16.0),
           child: Image.asset('assets/images/$image.png'),
         ),
         Padding(
-          padding: const EdgeInsets.all(48.0),
-          child: Text(text,
-              textAlign: TextAlign.center,
-              style:
-                  const TextStyle(fontSize: 18, fontWeight: FontWeight.w400)),
+          padding: const EdgeInsets.all(16.0),
+          child: SizedBox(
+            height: 40,
+            child: Text(text,
+                textAlign: TextAlign.center,
+                style:
+                    const TextStyle(fontSize: 14, fontWeight: FontWeight.w400)),
+          ),
         ),
       ],
     );
@@ -49,58 +58,53 @@ class _TutorialState extends State<Tutorial> {
       appBar: AppBar(
         title: const Text('Tutorial'),
       ),
-      body: Row(
+      body: Stack(
+        alignment: Alignment.center,
         children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: FloatingActionButton(
+          PageView(
+            controller: controller,
+            children: [
+              getPage('1select', 'Tap on a tile to select it.'),
+              getPage(
+                  '2move', 'Tap on any of the highlighted tiles to move.'),
+              getPage('3takepng', 'Tap on an enemy tile to take it.'),
+              getPage('4grave',
+                  'When a piece is taken, it goes to you graveyard.'),
+              getPage(
+                  '5revive', 'You can invoke pieces from your graveyard.'),
+              getPage('6knigthpng',
+                  'Reach to the top to transform a Pawn into a Knight.'),
+              getPage('7win', 'Take the king to win.'),
+              getPage('8clock',
+                  'But watch out your clock. If it gets to 0, you lose.'),
+              getPage('9moves', 'These are the valid moves.'),
+            ],
+          ),
+          Positioned(
+            left: 4,
+            child: FloatingActionButton(
                 backgroundColor: diablePrev ? Colors.grey : Colors.deepPurple,
-                  heroTag: 'back',
-                  mini: true,
-                  child: const Icon(Icons.arrow_left),
-                  onPressed: () {
-                    controller.previousPage(
-                        duration: const Duration(milliseconds: 200),
-                        curve: Curves.ease);
-                  }),
-            ),
+                heroTag: 'back',
+                mini: true,
+                child: const Icon(Icons.arrow_left),
+                onPressed: () {
+                  controller.previousPage(
+                      duration: const Duration(milliseconds: 200),
+                      curve: Curves.ease);
+                }),
           ),
-          Expanded(
-            child: PageView(
-              controller: controller,
-              children: [
-                getPage('1select', 'Tap on a tile to select it.'),
-                getPage(
-                    '2move', 'Tap on any of the highlighted tiles to move.'),
-                getPage('3takepng', 'Tap on an enemy tile to take it.'),
-                getPage('4grave',
-                    'When a piece is taken, it goes to you graveyard.'),
-                getPage(
-                    '5revive', 'You can invoke pieces from your graveyard.'),
-                getPage('6knigthpng',
-                    'Reach to the top to transform a Pawn into a Knight.'),
-                getPage('7win', 'Take the king to win.'),
-                getPage('8clock',
-                    'But watch out your clock. If it gets to 0, you lose.'),
-                getPage('9moves', 'These are the valid moves.'),
-              ],
-            ),
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: FloatingActionButton(
-                  backgroundColor: disableNext ? Colors.grey : Colors.deepPurple,
-                  heroTag: 'forward',
-                  mini: true,
-                  child: const Icon(Icons.arrow_right),
-                  onPressed: () {
-                    controller.nextPage(
-                        duration: const Duration(milliseconds: 200),
-                        curve: Curves.bounceIn);
-                  }),
-            ),
+          Positioned(
+            right: 4,
+            child: FloatingActionButton(
+                backgroundColor: disableNext ? Colors.grey : Colors.deepPurple,
+                heroTag: 'forward',
+                mini: true,
+                child: const Icon(Icons.arrow_right),
+                onPressed: () {
+                  controller.nextPage(
+                      duration: const Duration(milliseconds: 200),
+                      curve: Curves.bounceIn);
+                }),
           ),
         ],
       ),
