704f2c2be74ac3c04bb14273609de796733e047d
Josue Ortiz <jotakutsuki@gmail.com>
2023-08-23 13:54:45 -0500

    publish web and bumped version

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -2,71 +2,73 @@
 const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
-
-const RESOURCES = {"version.json": "5c5fedcdd693f87aeced0633f9b09804",
-"index.html": "40291ef1ff81556f697931182a960c1a",
-"/": "40291ef1ff81556f697931182a960c1a",
-"firebase-messaging-sw.js": "670eb0f6068de5465f1af94e471f4b99",
-"main.dart.js": "048ddc694ec5af9e88bfa0361e5a271c",
-"flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
-"favicon.png": "04db912f860ff064712ceb47f4136f97",
-"icons/Icon-192.png": "fcc2647ea7211a37e0562c8afdba0ebc",
-"icons/Icon-maskable-192.png": "fcc2647ea7211a37e0562c8afdba0ebc",
-"icons/Icon-maskable-512.png": "76e5bf89ef26d5a0e3693aa763f93486",
-"icons/Icon-512.png": "76e5bf89ef26d5a0e3693aa763f93486",
-"manifest.json": "f4ddb7ac10c0aacc928fd4d802deffcf",
-"assets/AssetManifest.json": "49377cfc2a95952f18980dd629fa929e",
-"assets/NOTICES": "e59cfaab767cdd4bcab6ee18302c6d30",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "5d9a03e26a917c898af2acbf3f91a5ad",
-"assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
-"assets/AssetManifest.smcbin": "b63233d9b081dccdf6bb07dae3498378",
-"assets/fonts/MaterialIcons-Regular.otf": "18432cd1ca2d7a8e97fd134efb5f8d53",
-"assets/assets/images/m2.png": "c35d548beea5e4da11a6466209e96da3",
-"assets/assets/images/icon.png": "7b250820d4c3f34da4c71db4bf78ca12",
-"assets/assets/images/m5.png": "15dc9fbc2fdd062ea6179172762bb24b",
-"assets/assets/images/6knigthpng.png": "348949549d827c0191667f6676cead96",
-"assets/assets/images/5revive.png": "1297235db737c9c4b4550767ec71d056",
-"assets/assets/images/pawnB.png": "9379023f5def61cf680f764cb7000d33",
-"assets/assets/images/3takepng.png": "269c42d80a78f3230acdbc25beedca42",
-"assets/assets/images/pawnW.png": "56c9aa252c1f006b22f18c953d486acd",
-"assets/assets/images/queenW.png": "dea010e2723d066c481a6d5d56fea281",
-"assets/assets/images/tutorial.psd": "47fbb255ff065eaf9ab921e8954bc9df",
-"assets/assets/images/8clock.png": "d7892101fec9f171e8b06cce0a76802e",
-"assets/assets/images/queenB.png": "e28adf842b2f45c82ebffdc6f7b24431",
+const RESOURCES = {
+  "assets/AssetManifest.json": "876921cd36d848bead4002a66da92fc1",
+"assets/assets/icon/banner.png": "9a25f65d70e0ce63b26244b1444ac299",
+"assets/assets/icon/icon.png": "7b250820d4c3f34da4c71db4bf78ca12",
+"assets/assets/icon/icon512.png": "ab1abb99ed61990c1f37b43ec41649f5",
 "assets/assets/images/1select.png": "eaf5df943d87d3d006f734f8679d9171",
+"assets/assets/images/2move.png": "5eba5dcb9e8caa385502ff5d7ae9a7ad",
+"assets/assets/images/3takepng.png": "269c42d80a78f3230acdbc25beedca42",
+"assets/assets/images/4grave.png": "2ac72b13e3b1860b30ebd659af2e4689",
+"assets/assets/images/5revive.png": "1297235db737c9c4b4550767ec71d056",
+"assets/assets/images/6knigthpng.png": "348949549d827c0191667f6676cead96",
+"assets/assets/images/7win.png": "e41464d09f8f1f212b6905118fb21416",
+"assets/assets/images/8clock.png": "d7892101fec9f171e8b06cce0a76802e",
+"assets/assets/images/9moves.png": "896538dcce5781658916d9ab09b5c25e",
+"assets/assets/images/avatar.png": "f4e0bcf3c42ed46c0f7c3aeb061d4d84",
+"assets/assets/images/bishopB.png": "f8b7fd33142018c5a6da6c5355c0d1fa",
+"assets/assets/images/bishopW.png": "f356fb25fb9aa54b024bfc81a490bb3f",
+"assets/assets/images/board.png": "509c5ce76ba0d682bc5c36d15228f1b1",
+"assets/assets/images/debug.log": "d3eeeef319833518109e57ae52b205b0",
+"assets/assets/images/glogo.png": "85346762215169a2f0013f2283bf56e4",
+"assets/assets/images/icon.png": "7b250820d4c3f34da4c71db4bf78ca12",
 "assets/assets/images/kingB.png": "ebeb5a958d79282276ebc1dc14519464",
+"assets/assets/images/kingW.png": "e7c3b688ecd6e180e3c25f31982b9166",
+"assets/assets/images/knightB.png": "710497377685773edc29dd1c3dbd0a8f",
+"assets/assets/images/knightW.png": "cb9f86a206989b04b7171d21dbb8795a",
+"assets/assets/images/m2.png": "c35d548beea5e4da11a6466209e96da3",
+"assets/assets/images/m3psd.png": "43b8eef12b827feeb7c5175bbb202350",
+"assets/assets/images/m4png.png": "7f96317a0bfe5a6d01c0931c6c82c4f4",
+"assets/assets/images/m5.png": "15dc9fbc2fdd062ea6179172762bb24b",
+"assets/assets/images/pawnB.png": "9379023f5def61cf680f764cb7000d33",
+"assets/assets/images/pawnW.png": "56c9aa252c1f006b22f18c953d486acd",
+"assets/assets/images/placeholder.png": "1632e46a5c79d43f3125ca62c54189cb",
+"assets/assets/images/queenB.png": "e28adf842b2f45c82ebffdc6f7b24431",
+"assets/assets/images/queenW.png": "dea010e2723d066c481a6d5d56fea281",
 "assets/assets/images/rockB.png": "f7d8987d4b71073b84292914d1480f0f",
 "assets/assets/images/rockW.png": "1e9f7568b625c03908a9918b83c016c6",
-"assets/assets/images/kingW.png": "e7c3b688ecd6e180e3c25f31982b9166",
 "assets/assets/images/selected.png": "615b0379ed0c30b25f82d1988c31be19",
-"assets/assets/images/placeholder.png": "1632e46a5c79d43f3125ca62c54189cb",
-"assets/assets/images/knightB.png": "710497377685773edc29dd1c3dbd0a8f",
-"assets/assets/images/m4png.png": "7f96317a0bfe5a6d01c0931c6c82c4f4",
-"assets/assets/images/knightW.png": "cb9f86a206989b04b7171d21dbb8795a",
-"assets/assets/images/4grave.png": "2ac72b13e3b1860b30ebd659af2e4689",
-"assets/assets/images/bishopB.png": "f8b7fd33142018c5a6da6c5355c0d1fa",
-"assets/assets/images/board.png": "509c5ce76ba0d682bc5c36d15228f1b1",
-"assets/assets/images/bishopW.png": "f356fb25fb9aa54b024bfc81a490bb3f",
-"assets/assets/images/9moves.png": "896538dcce5781658916d9ab09b5c25e",
-"assets/assets/images/m3psd.png": "43b8eef12b827feeb7c5175bbb202350",
-"assets/assets/images/glogo.png": "85346762215169a2f0013f2283bf56e4",
-"assets/assets/images/7win.png": "e41464d09f8f1f212b6905118fb21416",
-"assets/assets/images/2move.png": "5eba5dcb9e8caa385502ff5d7ae9a7ad",
-"canvaskit/skwasm.js": "1df4d741f441fa1a4d10530ced463ef8",
-"canvaskit/skwasm.wasm": "6711032e17bf49924b2b001cef0d3ea3",
-"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b5652e05",
-"canvaskit/chromium/canvaskit.wasm": "fc18c3010856029414b70cae1afc5cd9",
-"canvaskit/canvaskit.js": "76f7d822f42397160c5dfc69cbc9b2de",
-"canvaskit/canvaskit.wasm": "f48eaf57cada79163ec6dec7929486ea",
-"canvaskit/skwasm.worker.js": "19659053a277272607529ef87acf9d8a"};
+"assets/assets/images/tutorial.psd": "47fbb255ff065eaf9ab921e8954bc9df",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
+"assets/NOTICES": "2c1fccfe0a6d52463cf80cbe2d6b6737",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"favicon.png": "04db912f860ff064712ceb47f4136f97",
+"firebase-messaging-sw.js": "8f733150b8f1c6754319099cd3b75921",
+"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
+"icons/Icon-192.png": "fcc2647ea7211a37e0562c8afdba0ebc",
+"icons/Icon-512.png": "76e5bf89ef26d5a0e3693aa763f93486",
+"icons/Icon-maskable-192.png": "fcc2647ea7211a37e0562c8afdba0ebc",
+"icons/Icon-maskable-512.png": "76e5bf89ef26d5a0e3693aa763f93486",
+"index.html": "513dc0f23d548a9a7288aa09d509143c",
+"/": "513dc0f23d548a9a7288aa09d509143c",
+"main.dart.js": "90d690e6a34f9aa1a21a805a76f66208",
+"manifest.json": "d4e8251ce9c21a0d6a1a85beb4c41a3d",
+"version.json": "5c5fedcdd693f87aeced0633f9b09804"
+};
+
 // The application shell files that are downloaded before a service worker can
 // start.
-const CORE = ["main.dart.js",
+const CORE = [
+  "main.dart.js",
 "index.html",
 "assets/AssetManifest.json",
 "assets/FontManifest.json"];
-
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
   self.skipWaiting();
@@ -77,6 +79,7 @@ self.addEventListener("install", (event) => {
     })
   );
 });
+
 // During activate, the cache is populated with the temp files downloaded in
 // install. If this service worker is upgrading from one with a saved
 // MANIFEST, then use this to retain unchanged resource files.
@@ -98,8 +101,6 @@ self.addEventListener("activate", function(event) {
         await caches.delete(TEMP);
         // Save the manifest to make future upgrades efficient.
         await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
-        // Claim client to enable caching on first launch
-        self.clients.claim();
         return;
       }
       var oldManifest = await manifest.json();
@@ -125,8 +126,6 @@ self.addEventListener("activate", function(event) {
       await caches.delete(TEMP);
       // Save the manifest to make future upgrades efficient.
       await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
-      // Claim client to enable caching on first launch
-      self.clients.claim();
       return;
     } catch (err) {
       // On an unhandled exception the state of the cache cannot be guaranteed.
@@ -137,6 +136,7 @@ self.addEventListener("activate", function(event) {
     }
   }());
 });
+
 // The fetch handler redirects requests for RESOURCE files to the service
 // worker cache.
 self.addEventListener("fetch", (event) => {
@@ -176,6 +176,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 self.addEventListener('message', (event) => {
   // SkipWaiting can be used to immediately activate a waiting service worker.
   // This will also require a page refresh triggered by the main worker.
@@ -188,6 +189,7 @@ self.addEventListener('message', (event) => {
     return;
   }
 });
+
 // Download offline will check the RESOURCES for all files not in the cache
 // and populate them.
 async function downloadOffline() {
@@ -208,6 +210,7 @@ async function downloadOffline() {
   }
   return contentCache.addAll(resources);
 }
+
 // Attempt to download the resource online before falling back to
 // the offline cache.
 function onlineFirst(event) {
